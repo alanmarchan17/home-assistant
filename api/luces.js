75fed7f7e882624ad5a86a411f42b9b0aa348c10
alanmarchan17 <alan.gmarchan@gmail.com>
2026-03-19 14:25:56 -0600

    Update luces.js

--- api/luces.js
+++ api/luces.js
@@ -1,12 +1,15 @@
-
-  
 export default async function handler(req, res) {
+  // 🔥 CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   try {
     const response = await fetch(
       "https://uxphxdnksb1vpwehpovjgga7ydmldnhv.ui.nabu.casa/api/states/sensor.contador_de_luces_prendidas",
       {
         headers: {
-          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI4OTJiN2ZmMjYwYzM0ZDY0YTU1MGEzYzk3NjA4YmRhYSIsImlhdCI6MTc3MzI1NTU1NiwiZXhwIjoyMDg4NjE1NTU2fQ.zJJPxQAlB9rliDdDcttRkeOLtRysFtxCInvT5ralqnE"
+          Authorization: "Bearer TU_TOKEN_AQUI"
         }
       }
     );
